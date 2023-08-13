2776f8bf6bd73c7ac36e1db23723295666aac5bc
Jacek Kosinski <kashiash@gmail.com>
2023-08-13 17:19:00 +0200

    Initial Commit

---
+++ SwiftUIMapKitDemo/SwiftUIMapKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapKitDemoApp.swift
+//  SwiftUIMapKitDemo
+//
+//  Created by Jacek Kosinski U on 13/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
